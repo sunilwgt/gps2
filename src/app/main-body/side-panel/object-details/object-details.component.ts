da3a7e002f685267d1acf42255bfe6f7ccee0da7
wgtAtul <atul.wgt@gmail.com>
2019-01-17 18:59:52 +0530

    headers changed

--- src/app/main-body/side-panel/object-details/object-details.component.ts
+++ src/app/main-body/side-panel/object-details/object-details.component.ts
@@ -2,12 +2,13 @@ import { Component, OnInit, Input } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { CommonServiceService } from './../../../service/common-service.service';
 import { RestService } from '../../../service/rest.service';
+import { ObjectDetails } from './object-details.model';
 
 
 
 var headers_object = new HttpHeaders();
 headers_object.append('Content-Type', 'application/json');
-headers_object.append("Authorization", "Basic" + "admin:admin");
+headers_object.append("Authorization", "Basic " + "YWRtaW46YWRtaW4=");
 
 const httpOptions = {
   withCredentials: true,
@@ -20,18 +21,18 @@ const httpOptions = {
 @Component({
   selector: 'app-object-details',
   templateUrl: './object-details.component.html',
-  styleUrls: ['./object-details.component.scss'],  
+  styleUrls: ['./object-details.component.scss'],
 })
 export class ObjectDetailsComponent implements OnInit {
 
-  constructor( public CommonService:CommonServiceService, private ajax:RestService ) { }
+  constructor(public CommonService: CommonServiceService, private ajax: RestService) { }
 
-  deviceDetails:object;
-  
-  
+  deviceDetails: ObjectDetails;
+
+
   ngOnInit() {
-      
-    this.CommonService.deviceDetails.subscribe((value)=>{      
+
+    this.CommonService.deviceDetails.subscribe((value) => {
       this.deviceDetails = value[0];
       //console.log (value);
     })
@@ -46,7 +47,7 @@ export class ObjectDetailsComponent implements OnInit {
 
 
   status: boolean = false;
-  public showhide(){
+  public showhide() {
     this.status = !this.status;
   }
 
